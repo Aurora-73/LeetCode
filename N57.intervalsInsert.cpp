6253655f1758f56c575a0b36b53d123eb6b0c46d
Aurora-73 <c2394842823@gmail.com>
2025-04-22 22:03:02 +0800

    Commit on 2025/04/22 周二

--- N57.intervalsInsert.cpp
+++ N57.intervalsInsert.cpp
@@ -1,35 +1,51 @@
 #include "MyUtils.h"
 
-class Solution {
+class Solution1 {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        auto lb = lower_bound(intervals.begin(), intervals.end(), newInterval, [](vector<int> a, vector<int> b){return a[0] < b[0];});
-        auto ub = upper_bound(intervals.begin(), intervals.end(), newInterval, [](vector<int> a, vector<int> b){return a[1] < b[1];});
-        vector<int> &before = *(lb - 1), &after = *lb;
-        bool done = false;
-        if(before[1] >= newInterval[0]){
-            before[1] = max(before[1], newInterval[1]);
-            done = true;
-        }
-        if(after[0] <= newInterval[1]){
-            after[0] = min(after[0], newInterval[1]);
-            if(done){
-                before[1] = max(before[1], after[1]);
-                intervals.erase(lb);
-            }
-            done = true;
-        }
-        
-        if(!done){
-            intervals.insert(lb, newInterval);
-        }
+        vector<vector<int>> newIntervals; // 不设置新数组很难写
+        int i = 0;
+        while(i < intervals.size() && intervals[i][1] < newInterval[0]){
+            newIntervals.push_back(intervals[i]);
+            i++;
+        } // 先插入没有重叠的
+        newIntervals.push_back(newInterval);
+        while(i < intervals.size() && intervals[i][0] <= newInterval[1]){
+            newIntervals.back()[0] = min(newIntervals.back()[0], intervals[i][0]);
+            newIntervals.back()[1] = max(newIntervals.back()[1], intervals[i][1]);
+            i++;
+        } // 处理重叠的
+        while(i < intervals.size()){ 
+            newIntervals.push_back(intervals[i]);
+            i++;
+        } // 再插入没有重叠的
+        return newIntervals;
+    }
+};
+
+class Solution2 {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i = 0;
+        while(i < intervals.size() && intervals[i][1] < newInterval[0]) i++; // 先跳过没有重叠的
+
+        intervals.insert(intervals.begin() + i, newInterval);
+        if(i < intervals.size() - 1)    intervals[i][0] = min(intervals[i][0], intervals[i + 1][0]); 
+        // 将自身插入进去，必须插入，为了归一化后面为空的情况，同时调整自身的最小值
+
+        while(i + 1 < intervals.size() && intervals[i][1] >= intervals[i + 1][0]){
+            intervals[i][1] = max(intervals[i][1], intervals[i + 1][1]);
+            intervals.erase(intervals.begin() + i + 1);
+        } // 处理重叠的，吃掉后面和自己重叠的
         return intervals;
     }
 };
     
 int main(){
-    Solution sol;
+    Solution1 sol1;
+    Solution2 sol2;
     vector<vector<int>> intervals = {{1,3},{6,9}};
     vector<int> newInterval = {2,5};
-    cout << sol.insert(intervals, newInterval);
+    cout << sol1.insert(intervals, newInterval) << endl;
+    cout << sol2.insert(intervals, newInterval) << endl;
 }
