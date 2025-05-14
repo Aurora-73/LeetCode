2383fd4d1302bc81de1506616b7ff9b9583d3ae7
Aurora-73 <c2394842823@gmail.com>
2025-05-15 00:39:09 +0800

    Commit on 2025/05/15 周四

--- 74N.searchMatrix.cpp
+++ 74N.searchMatrix.cpp
@@ -2,7 +2,7 @@
 #include "MyUtils.h"
 
 /*74. 搜索二维矩阵
-给你一个满足下述两条属性的 m x n 整数矩阵：
+给你一个满足下述两条属性的 cols x rows 整数矩阵：
 每行中的整数从左到右按非严格递增顺序排列。
 每行的第一个整数大于前一行的最后一个整数。
 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。*/
@@ -14,22 +14,22 @@ public:
 	bool searchMatrix(vector<vector<int>> &matrix, int target) {
 		if(matrix.empty() || matrix[0].empty())
 			return false;
-		int n = matrix.size(), m = matrix[0].size(); // 不一定是方形
-		int left_i = 0, left_j = 0, right_i = n - 1, right_j = m - 1; // 左闭右闭
-		while(left_i * m + left_j <= right_i * m + right_j) {
-			int middle = ((right_i - left_i) * m + right_j - left_j) / 2 + left_i * m + left_j;
-			int mid_i = middle / m, mid_j = middle % m;
+		int rows = matrix.size(), cols = matrix[0].size(); // 不一定是方形
+		int left_i = 0, left_j = 0, right_i = rows - 1, right_j = cols - 1; // 左闭右闭
+		while(left_i * cols + left_j <= right_i * cols + right_j) {
+			int middle = ((right_i - left_i) * cols + right_j - left_j) / 2 + left_i * cols + left_j;
+			int mid_i = middle / cols, mid_j = middle % cols;
 			int &mid_val = matrix[mid_i][mid_j];
 			if(target == mid_val) {
 				return true;
 			} else if(target < mid_val) {
 				middle -= 1; // 直接给 right_j ± 1 可能导致进位或者 -1 的情况，必须重新计算
-				right_i = middle / m;
-				right_j = middle % m;
+				right_i = middle / cols;
+				right_j = middle % cols;
 			} else {
 				middle += 1;
-				left_i = middle / m;
-				left_j = middle % m;
+				left_i = middle / cols;
+				left_j = middle % cols;
 			}
 		}
 		return false;
@@ -40,11 +40,11 @@ public:
 class Solution11 {
 public:
 	bool searchMatrix(vector<vector<int>> &matrix, int target) {
-		int n = matrix.size(), m = matrix[0].size();
-		int left = 0, right = n * m - 1, mid, mid_val;
+		int rows = matrix.size(), cols = matrix[0].size();
+		int left = 0, right = rows * cols - 1, mid, mid_val;
 		while(left <= right) {
 			mid = left + (right - left) / 2;
-			mid_val = matrix[mid / m][mid % m];
+			mid_val = matrix[mid / cols][mid % cols];
 			if(target == mid_val) {
 				return true;
 			} else if(target < mid_val) {
@@ -63,11 +63,11 @@ public:
 	bool searchMatrix(vector<vector<int>> &matrix, int target) {
 		if(matrix.empty() || matrix[0].empty())
 			return false;
-		int n = matrix.size(), m = matrix[0].size(); // 不一定是方形
-		int left_i = 0, left_j = 0, right_i = n - 1, right_j = m; // 左闭右开
-		while(left_i * m + left_j < right_i * m + right_j) {
-			int middle = ((right_i - left_i) * m + right_j - left_j) / 2 + left_i * m + left_j;
-			int mid_i = middle / m, mid_j = middle % m;
+		int rows = matrix.size(), cols = matrix[0].size(); // 不一定是方形
+		int left_i = 0, left_j = 0, right_i = rows - 1, right_j = cols; // 左闭右开
+		while(left_i * cols + left_j < right_i * cols + right_j) {
+			int middle = ((right_i - left_i) * cols + right_j - left_j) / 2 + left_i * cols + left_j;
+			int mid_i = middle / cols, mid_j = middle % cols;
 			int &mid_val = matrix[mid_i][mid_j];
 			if(target == mid_val) {
 				return true;
@@ -76,8 +76,8 @@ public:
 				right_j = mid_j;
 			} else {
 				middle += 1; // 直接给 right_j ± 1 可能导致进位或者 -1 的情况，必须重新计算  // 注意这里仍要加1
-				left_i = middle / m;
-				left_j = middle % m;
+				left_i = middle / cols;
+				left_j = middle % cols;
 			}
 		}
 		return false;
@@ -88,11 +88,11 @@ public:
 class Solution21 {
 public:
 	bool searchMatrix(vector<vector<int>> &matrix, int target) {
-		int n = matrix.size(), m = matrix[0].size();
-		int left = 0, right = n * m, mid, mid_val;
+		int rows = matrix.size(), cols = matrix[0].size();
+		int left = 0, right = rows * cols, mid, mid_val;
 		while(left < right) {
 			mid = left + (right - left) / 2;
-			mid_val = matrix[mid / m][mid % m];
+			mid_val = matrix[mid / cols][mid % cols];
 			if(target == mid_val) {
 				return true;
 			} else if(target < mid_val) {
@@ -105,6 +105,40 @@ public:
 	}
 };
 
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int>> &matrix, int target) {
+		if(matrix.empty() || matrix[0].empty())
+			return false;
+		int rows = matrix.size(), up = 0, down = rows; // 左闭右开
+		while(up < down) {
+			int mid = up + (down - up) / 2, mid_val = matrix[mid].back();
+			if(mid_val == target) {
+				return true;
+			} else if(mid_val > target) {
+				down = mid;
+			} else {
+				up = mid + 1;
+			}
+		} // 二分查找结束时的当前值大于等于target
+		if(up == rows) { // 在这里等价于 up < 0 || up > rows - 1
+			return false;
+		}
+		int cols = matrix[0].size(), left = 0, right = cols; // 左闭右开
+		while(left < right) {
+			int mid = left + (right - left) / 2, mid_val = matrix[up][mid];
+			if(mid_val == target) {
+				return true;
+			} else if(mid_val > target) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return false;
+	}
+}; // O(logn+logm)
+
 int main() {
-	Solution11 sol11;
+	Solution sol;
 }
