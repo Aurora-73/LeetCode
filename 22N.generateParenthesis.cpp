78d61b438f21e2575dfc195061469f94c1195904
Aurora <146014849+Aurora-73@users.noreply.github.com>
2025-05-15 23:08:39 +0800

    Commit on 2025/05/15 周四

--- 22N.generateParenthesis.cpp
+++ 22N.generateParenthesis.cpp
@@ -4,19 +4,20 @@
 /*22. 括号生成
 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。*/
 
-class Solution {
+class Solution1 {
 public:
 	vector<string> generateParenthesis(int n) {
 		size = 2 * n;
 		temp.resize(size);
-		res.reserve(res_size[n]);
+		int *aa = new int[9]{0, 1, 3, 8, 22, 64, 196, 625, 2055};
+		res.reserve(aa[n]);
+		delete[] aa;
 		dfs();
-		return res;
+		return std::move(res);
 	}
 
 private:
 	vector<string> res;
-	int res_size[9] = { 0, 1, 3, 8, 22, 64, 196, 625, 2055 };
 	string temp;
 	int top = 0, left = 0, size;
 	void dfs() {
@@ -39,13 +40,16 @@ private:
 	}
 };
 
-class Solution {
+class Solution2 {
 public:
 	vector<string> generateParenthesis(int n) {
 		vector<string> res;
+		int *aa = new int[9]{0, 1, 3, 8, 22, 64, 196, 625, 2055};
+		res.reserve(aa[n]);
+		delete[] aa;
 		string temp(2 * n, ' ');
 		dfs(res, temp, 0, 0, n);
-		return res;
+		return std::move(res);
 	}
 
 private:
@@ -72,8 +76,8 @@ private:
 };
 
 int main() {
-	Solution sol;
-	for(int i = 1; i < 9; ++i) {
-		cout << sol.generateParenthesis(i) << ",";
+	Solution1 sol1;
+	for(int i = 1; i < 4; ++i) {
+		cout << sol1.generateParenthesis(i) << endl;
 	}
 }
