74d6234ad158b5c5a984339ca33a0146372b0eb4
Aurora-73 <c2394842823@gmail.com>
2025-05-21 22:55:44 +0800

    Commit on 2025/05/21 周三

--- My.binarySearch.cpp
+++ My.binarySearch.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int binarySearch(const vector<int> &vec, int key) {
 	int left = 0, right = vec.size(), middle; // 左闭右开, right 为 size
-	while(left < right) { // 左闭右开 left == right 为空区间
+	while(left < right) {                     // 左闭右开 left == right 为空区间
 		middle = left + (right - left) / 2;
 		if(key == vec[middle]) {
 			return middle;
@@ -58,7 +58,8 @@ int find_first(const vector<int> &vec, Cond cond) {
 			left = mid + 1; // vec[mid] 不满足，答案在 mid+1…right
 		} else {
 			right = mid; // vec[mid] 满足，答案在 left…mid
-			// mid 满足，不舍弃（虽然在左闭右开的形式中舍弃了，但是返回时left = right其实没有舍弃，只是以后不在检查这个元素了）
+			             // mid 满足，不舍弃（虽然在左闭右开的形式中舍弃了，但是返回时left =
+			             // right其实没有舍弃，只是以后不在检查这个元素了）
 		}
 	}
 	return left;
@@ -67,17 +68,47 @@ int find_first(const vector<int> &vec, Cond cond) {
 // upper_bound是conde = [&](int val){return val > key;}
 // binary_search是conde = [&](int val){return val >= key;} 且 left < n && vec[left] == key
 
+template <typename RandomIt, typename Cond>
+RandomIt my_find_first(RandomIt first, RandomIt last, Cond cond) {
+	// 要求 RandomIt 支持 last - first
+	using diff_t = typename std::iterator_traits<RandomIt>::difference_type;
+	diff_t len = last - first; // 区间 [first, last) 长度
+
+	while(len > 0) {
+		diff_t half = len / 2;
+		RandomIt mid = first + half; // 计算中点
+		if(!cond(mid)) {
+			// [first..mid] 都不满足
+			first = mid + 1;
+			len = len - half - 1;
+		} else {
+			// mid 及其右侧可能满足
+			len = half;
+		}
+	}
+	// 退出时 first==last 或 *first 是第一个满足 cond 的位置
+	return first;
+} // 返回第一个使得 cond条件为真的元素的的迭代器, 分界线左边不满足，右边均满足，左闭右开
+// 标准格式，lower_bound是conde = [&](RandomIt::value_type val){return val >= key;}
+// upper_bound是conde = [&](RandomIt::value_type val){return val > key;}
+// binary_search是conde = [&](RandomIt::value_type val){return val >= key;} 且 first != end &&
+// *first == key 更好的写法应该是 std::iterator_traits<RandomIt>::value_type，因为 RandomIt
+// 可能是原生指针
+
 int main() {
 	vector<int> vec = { 0, 2, 4, 6, 8, 10 };
 	int key = 5;
 	cout << "left close right close " << binarySearch(vec, key) << endl;
 	cout << "lower_bound STL " << lower_bound(vec.begin(), vec.end(), key) - vec.begin() << endl;
 	cout << "lower_bound " << lower_bound(vec, key) << endl;
-	auto cond = [&](int val) { return val >= key; };
+	auto cond = [&](int val) {
+		return val >= key;
+	};
 	cout << "find_first " << find_first(vec, cond) << endl;
 	key = 4;
 	cout << "left close right close " << binarySearch(vec, key) << endl;
 	cout << "lower_bound STL " << lower_bound(vec.begin(), vec.end(), key) - vec.begin() << endl;
 	cout << "lower_bound " << lower_bound(vec, key) << endl;
-	cout << "find_first " << find_first(vec, cond) << endl; // key是以引用方式捕获，修改外部key会自动同步到内部
+	cout << "find_first " << find_first(vec, cond)
+	     << endl; // key是以引用方式捕获，修改外部key会自动同步到内部
 }
