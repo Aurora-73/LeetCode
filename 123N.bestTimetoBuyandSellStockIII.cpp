a42460467ccc1eb1919f3a8dcce6b6707538e42e
Aurora-73 <c2394842823@gmail.com>
2025-05-25 16:24:54 +0800

    Commit on 2025/05/25 周日

---
+++ 123N.bestTimetoBuyandSellStockIII.cpp
@@ -0,0 +1,16 @@
+// Created: 2025-05-25
+#include "MyUtils.h"
+
+/*  123. 买卖股票的最佳时机 III
+给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。*/
+
+class Solution {
+public:
+	int maxProfit(vector<int> &prices) { }
+};
+
+int main() {
+	Solution sol;
+}
