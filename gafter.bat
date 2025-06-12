e2e023d84799f3e2ef69c3584e39f4adbb8268f3
Aurora-73 <c2394842823@gmail.com>
2025-06-12 15:46:53 +0800

    Commit on 周四 2025/06/12

--- gafter.bat
+++ gafter.bat
@@ -1,6 +1,7 @@
+@echo off
+chcp 65001 >nul
 rmdir /s /q .history
 git add .
 set commit_message=Commit on %date%
-@REM git commit -m "%commit_message%"
-git commit -m "修改任务配置，使用动态链接库"
+git commit -m "%commit_message%"
 git push
