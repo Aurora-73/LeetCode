7ab5f1b293b367be22f37e9a2cbebad18ceca857
Aurora-73 <c2394842823@gmail.com>
2026-01-09 00:11:51 +0800

    Commit on 2026/01/09 周五

--- MST17.24.getMaxMatrix.cpp
+++ MST17.24.getMaxMatrix.cpp
@@ -53,7 +53,7 @@ public:
 	}
 };
 
-class Solution {
+class Solution2 {
 public:
 	vector<int> getMaxMatrix(vector<vector<int>> &matrix) {
 		int m = matrix.size(), n = matrix[0].size();
@@ -66,29 +66,132 @@ public:
 		}
 		for(int ej = 0; ej < n; ++ej) {
 			for(int bj = 0; bj <= ej; ++bj) {
-				vector<int> nums;
-				nums.reserve(m);
-				for(int i = 0; i < m; ++i) {
-					nums.push_back(matrix[i][ej] - (bj ? matrix[i][bj - 1] : 0));
-				}
 				// 降维之后求最大子数组，贪心的前缀和
 				int sum = 0, best_begin = 0, best_prev = 0;
 				for(int i = 0; i < m; ++i) {
-					sum += nums[i];
+					int curr = matrix[i][ej] - (bj ? matrix[i][bj - 1] : 0);
+					sum += curr;
 					if(sum - best_prev > max_sum) {
 						max_sum = sum - best_prev;
 						res = { best_begin, bj, i, ej };
 					}
 					if(sum < best_prev) {
 						best_prev = sum;
-						best_begin = i;
+						best_begin = i + 1;
 					}
 				}
 			}
 		}
 		return res;
 	}
-};
+}; // 先对每一行求前缀和，然后遍历开始和结束行，通过前缀和求出每一行的部分和，然后在部分和的维数组上求最大子数组 O(n^2 * m)
+
+class Solution3 {
+public:
+	vector<int> getMaxMatrix(vector<vector<int>> &matrix) {
+		int m = matrix.size(), n = matrix[0].size();
+		int max_sum = matrix[0][0];
+		vector<int> res { 0, 0, 0, 0 };
+		// 对每一列做前缀和
+		for(int j = 0; j < n; ++j) {
+			for(int i = 1; i < m; ++i) {
+				matrix[i][j] += matrix[i - 1][j];
+			}
+		}
+		// 固定上边界和下边界，降维为一维最大子数组
+		for(int ei = 0; ei < m; ++ei) {
+			for(int bi = 0; bi <= ei; ++bi) {
+				int sum = 0, best_begin = 0, best_prev = 0;
+				for(int j = 0; j < n; ++j) {
+					int curr = matrix[ei][j] - (bi ? matrix[bi - 1][j] : 0);
+					sum += curr;
+					if(sum - best_prev > max_sum) {
+						max_sum = sum - best_prev;
+						res = { bi, best_begin, ei, j };
+					}
+					if(sum < best_prev) {
+						best_prev = sum;
+						best_begin = j + 1;
+					}
+				}
+			}
+		}
+		return res;
+	}
+}; // 先对每一列求前缀和，然后遍历开始和结束列，通过前缀和求出每一列的部分和，然后在部分和的维数组上求最大子数组 O(m^2 * n)
+
+class Solution {
+public:
+	vector<int> getMaxMatrix(vector<vector<int>> &matrix) {
+		int m = matrix.size(), n = matrix[0].size();
+		if(m > n) {
+			return nSmall(matrix);
+		} else {
+			return mSmall(matrix);
+		}
+	}
+
+private:
+	vector<int> nSmall(vector<vector<int>> &matrix) {
+		int m = matrix.size(), n = matrix[0].size();
+		int max_sum = matrix[0][0];
+		vector<int> res { 0, 0, 0, 0 };
+		for(int i = 0; i < m; ++i) {
+			for(int j = 1; j < n; ++j) {
+				matrix[i][j] += matrix[i][j - 1];
+			}
+		}
+		for(int ej = 0; ej < n; ++ej) {
+			for(int bj = 0; bj <= ej; ++bj) {
+				// 降维之后求最大子数组，贪心的前缀和
+				int sum = 0, best_begin = 0, best_prev = 0;
+				for(int i = 0; i < m; ++i) {
+					int curr = matrix[i][ej] - (bj ? matrix[i][bj - 1] : 0);
+					sum += curr;
+					if(sum - best_prev > max_sum) {
+						max_sum = sum - best_prev;
+						res = { best_begin, bj, i, ej };
+					}
+					if(sum < best_prev) {
+						best_prev = sum;
+						best_begin = i + 1;
+					}
+				}
+			}
+		}
+		return res;
+	}
+	vector<int> mSmall(vector<vector<int>> &matrix) {
+		int m = matrix.size(), n = matrix[0].size();
+		int max_sum = matrix[0][0];
+		vector<int> res { 0, 0, 0, 0 };
+		// 对每一列做前缀和
+		for(int j = 0; j < n; ++j) {
+			for(int i = 1; i < m; ++i) {
+				matrix[i][j] += matrix[i - 1][j];
+			}
+		}
+		// 固定上边界和下边界，降维为一维最大子数组
+		for(int ei = 0; ei < m; ++ei) {
+			for(int bi = 0; bi <= ei; ++bi) {
+				int sum = 0, best_begin = 0, best_prev = 0;
+				for(int j = 0; j < n; ++j) {
+					int curr = matrix[ei][j] - (bi ? matrix[bi - 1][j] : 0);
+					sum += curr;
+					if(sum - best_prev > max_sum) {
+						max_sum = sum - best_prev;
+						res = { bi, best_begin, ei, j };
+					}
+					if(sum < best_prev) {
+						best_prev = sum;
+						best_begin = j + 1;
+					}
+				}
+			}
+		}
+		return res;
+	}
+}; // 前两个的结合，根据数组的维度选择最小的那个
 
 int main() {
 	Solution sol;
