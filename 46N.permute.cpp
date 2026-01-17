953e1ba9d3281795ac67cce7c459593b60ee7d85
Aurora-73 <c2394842823@gmail.com>
2026-01-18 02:00:06 +0800

    Commit on 2026/01/18 周日

--- 46N.permute.cpp
+++ 46N.permute.cpp
@@ -1,8 +1,22 @@
 // Created: 2025-05-12
 #include "MyUtils.hpp"
 
-/*46. 全排列
-给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。*/
+/* 46. 全排列
+给定一个不含重复数字的数组 nums，返回其 所有可能的全排列。
+你可以 按任意顺序 返回答案。
+示例 1：
+	输入：nums = {1,2,3}
+	输出：{{1,2,3},{1,3,2},{2,1,3},{2,3,1},{3,1,2},{3,2,1}}
+示例 2：
+	输入：nums = {0,1}
+	输出：{{0,1},{1,0}}
+示例 3：
+	输入：nums = {1}
+	输出：{{1}}
+提示：
+	1 <= nums.length <= 6
+	-10 <= nums{i} <= 10
+	nums 中的所有整数 互不相同 */
 
 class Solution1 {
 public:
@@ -47,42 +61,6 @@ private:
 class Solution2 {
 public:
 	vector<vector<int>> permute(vector<int> &nums) {
-		n = nums.size();
-		temp.resize(n);
-		in_temp = vector<bool>(n, false);
-		size_t fact = 1;
-		for(int i = 2; i <= n; ++i) fact *= i;
-		res.reserve(fact);
-		top = 0;
-		dfs(nums, 0); // 只传递递归深度
-		return res;
-	}
-
-private:
-	vector<vector<int>> res;
-	vector<bool> in_temp;
-	vector<int> temp;
-	int n, top;
-	void dfs(const vector<int> &nums, int depth) {
-		if(depth == n) {
-			res.push_back(temp); // 此时 top == n
-			return;
-		}
-		for(int i = 0; i < n; ++i) {
-			if(!in_temp[i]) {
-				temp[top++] = nums[i];
-				in_temp[i] = true;
-				dfs(nums, depth + 1);
-				--top; // 回溯
-				in_temp[i] = false;
-			}
-		}
-	}
-};
-
-class Solution3 {
-public:
-	vector<vector<int>> permute(vector<int> &nums) {
 		int n = nums.size();
 		// 预分配结果空间，n! 个排列
 		size_t fact = 1;
@@ -109,8 +87,39 @@ private:
 	}
 }; // 去掉标记数组
 
+class Solution {
+public:
+	vector<vector<int>> permute(vector<int> &nums) {
+		n = nums.size();
+		curr = std::vector<int>(n);
+		visited = std::vector<bool>(n);
+		dfs(0, nums);
+		return std::move(res);
+	}
+
+private:
+	int n;
+	vector<vector<int>> res;
+	vector<int> curr;
+	vector<bool> visited;
+	void dfs(int i, vector<int> &nums) {
+		if(i == n) {
+			res.push_back(curr);
+			return;
+		}
+		for(int j = 0; j < n; ++j) {
+			if(!visited[j]) {
+				curr[i] = nums[j];
+				visited[j] = 1;
+				dfs(i + 1, nums);
+				visited[j] = 0;
+			}
+		}
+	}
+};
+
 int main() {
-	Solution2 sol;
+	Solution sol;
 	vector<int> vec;
 	cout << sol.permute(vec = { 1, 2, 3, 4 });
 }
