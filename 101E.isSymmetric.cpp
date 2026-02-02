8942f2eafa844cbca58c236be10b9b70f74f61d7
Aurora-73 <c2394842823@gmail.com>
2026-02-03 02:00:03 +0800

    Commit on 2026/02/03 周二

--- 101E.isSymmetric.cpp
+++ 101E.isSymmetric.cpp
@@ -1,23 +1,76 @@
 #include "MyTreeNode.h"
 #include "MyUtils.hpp"
 
+/* 给你一个二叉树的根节点 root， 检查它是否轴对称。
+示例 1：
+	输入：root = {1,2,2,3,4,4,3}
+	输出：true
+示例 2：
+	输入：root = {1,2,2,null,3,null,3}
+	输出：false
+提示：
+	树中节点数目在范围 {1, 1000} 内
+	-100 <= Node.val <= 100
+	进阶：你可以运用递归和迭代两种方法解决这个问题吗？ */
+
+class Solution1 {
+public:
+	bool isSymmetric(TreeNode *root) {
+		res = true;
+		if(root) isEqual(root->left, root->right);
+		return res;
+	}
+
+private:
+	bool res;
+	void isEqual(TreeNode *a, TreeNode *b) {
+		if(!res) return;
+		if(!a && !b) return;
+		if(!a || !b || a->val != b->val) {
+			res = 0;
+			return;
+		}
+		isEqual(a->left, b->right);
+		isEqual(a->right, b->left);
+	}
+}; // 递归写法
+
 class Solution {
 public:
 	bool isSymmetric(TreeNode *root) {
 		if(!root) return true;
-		return ismirror(root->left, root->right);
+
+		queue<TreeNode *> q;
+		q.push(root->left);
+		q.push(root->right);
+
+		while(!q.empty()) {
+			TreeNode *left = q.front();
+			q.pop();
+			TreeNode *right = q.front();
+			q.pop();
+
+			if(!left && !right) continue;
+			if(!left || !right) return false;
+			if(left->val != right->val) return false;
+
+			// 按“镜像”顺序入队
+			q.push(left->left);
+			q.push(right->right);
+			q.push(left->right);
+			q.push(right->left);
+		}
+
+		return true;
 	}
-	bool ismirror(TreeNode *a, TreeNode *b) {
-		if(!a && !b) return true;
-		if(!a || !b) return false;
-		return a->val == b->val && ismirror(a->left, b->right) && ismirror(a->right, b->left);
-	} // 如果是镜像，则将a->left替换掉a, b->right替换掉b仍是镜像，反之亦然
-};
+}; // 迭代写法
 
 int main() {
-	Solution s;
-	TreeNode *root = createTree({ 1, 2, 2, 3, 4, 4, 3 });
-	cout << root << endl;
-	std::cout << "Is symmetric: " << std::boolalpha << s.isSymmetric(root) << std::endl;
-	return 0;
+	Solution sol;
+	TreeNode *root;
+	root = createTree({ 1, 2, 2, 3, 4, 4, 3 });
+	cout << sol.isSymmetric(root) << endl;
+
+	root = createTree({ 1, 2, 2, EMPTY_NODE, 3, EMPTY_NODE, 3 });
+	cout << sol.isSymmetric(root) << endl;
 }
--- 394N.DecodeString.cpp
+++ 394N.DecodeString.cpp
@@ -3,31 +3,31 @@
 #include "MyUtils.hpp"
 // #ifdef MY_UTILS_H
 
-/* 394. 字符串解码
-给定一个经过编码的字符串，返回它解码后的字符串。
-编码规则为: k{encoded_string}，
+/* 给定一个经过编码的字符串，返回它解码后的字符串。
+编码规则为: k[encoded_string]，
 表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
 你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
-此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k，例如不会出现像3a或2{4}的输入。
+此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k，例如不会出现像3a或2[4]的输入。
+测试用例保证输出的长度不会超过10^5。
 示例 1：
-	输入：s = "3{a}2{bc}"
+	输入：s = "3[a]2[bc]"
 	输出："aaabcbc"
 示例 2：
-	输入：s = "3{a2{c}}"
+	输入：s = "3[a2[c]]"
 	输出："accaccacc"
 示例 3：
-	输入：s = "2{abc}3{cd}ef"
+	输入：s = "2[abc]3[cd]ef"
 	输出："abcabccdcdcdef"
 示例 4：
-	输入：s = "abc3{cd}xyz"
+	输入：s = "abc3[cd]xyz"
 	输出："abccdcdcdxyz"
 提示：
 	1 <= s.length <= 30
-	s由小写英文字母、数字和方括号"{}" 组成
+	s由小写英文字母、数字和方括号"[]" 组成
 	s保证是一个有效的输入。
-	s中所有整数的取值范围为{1, 300} */
+	s中所有整数的取值范围为[1, 300] */
 
-class Solution {
+class Solution1 {
 public:
 	string decodeString(string &s) {
 		string res;
@@ -68,15 +68,56 @@ public:
 	}
 }; // 类似于括号匹配算法，用栈解决
 
+class Solution {
+public:
+	string decodeString(string &s) {
+		string res;
+		stack<pair<int, string>> st;
+		int n = s.size(), i = 0;
+		while(i < n) {
+			if(isdigit(s[i])) {
+				int cnt = 0;
+				while(s[i] != '[') {
+					cnt = cnt * 10 + s[i++] - '0';
+				}
+				st.emplace(cnt, "");
+			} else if(s[i] == ']') {
+				auto [cnt, curr] = std::move(st.top());
+				st.pop();
+				if(st.empty()) {
+					while(cnt--) {
+						res += curr;
+					}
+				} else {
+					while(cnt--) {
+						st.top().second += curr;
+					}
+				}
+			} else if(s[i] != '[') {
+				if(st.empty()) {
+					res.push_back(s[i]);
+				} else {
+					st.top().second.push_back(s[i]);
+				}
+			}
+			++i;
+		}
+		return res;
+	}
+};
+
 int main() {
 	Solution sol;
 	string s;
 	s = "3[a]2[bc]";
 	cout << sol.decodeString(s) << endl; // "aaabcbc"
+
 	s = "3[a2[c]]";
 	cout << sol.decodeString(s) << endl; // "accaccacc"
+
 	s = "2[abc]3[cd]ef";
 	cout << sol.decodeString(s) << endl; // "abcabccdcdcdef"
+
 	s = "abc3[cd]xyz";
 	cout << sol.decodeString(s) << endl; // "abccdcdcdxyz"
 }
