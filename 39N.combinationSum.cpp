438332baf8838124218757d8bf170907b28bb40f
Aurora-73 <c2394842823@gmail.com>
2026-02-04 02:00:05 +0800

    Commit on 2026/02/04 周三

--- 39N.combinationSum.cpp
+++ 39N.combinationSum.cpp
@@ -1,50 +1,111 @@
 // Created: 2025-05-12
 #include "MyUtils.hpp"
 
-/*39. 组合总和
-给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，
-并以列表形式返回。你可以按 任意顺序 返回这些组合。
-candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
-对于给定的输入，保证和为 target 的不同组合数少于 150 个。*/
+/* 39. 组合总和
+给你一个 无重复元素 的整数数组candidates 和一个目标整数target，
+找出candidates中可以使数字和为目标数target 的 所有不同组合，并以列表形式返回。
+你可以按 任意顺序 返回这些组合。
+candidates 中的 同一个 数字可以 无限制重复被选取。如果至少一个数字的被选数量不同，则两种组合是不同的。
+对于给定的输入，保证和为target 的不同组合数少于 150 个。
+示例1：
+	输入：candidates = {2,3,6,7}, target = 7
+	输出：{{2,2,3},{7}}
+	解释：
+	2 和 3 可以形成一组候选，2 + 2 + 3 = 7。注意 2 可以使用多次。
+	7 也是一个候选， 7 = 7。
+	仅有这两种组合。
+示例2：
+	输入: candidates = {2,3,5}, target = 8
+	输出: {{2,2,2,2},{2,3,3},{3,5}}
+示例 3：
+	输入: candidates = {2}, target = 1
+	输出: {}
+提示：
+	1 <= candidates.length <= 30
+	2 <= candidates{i} <= 40
+	candidates 的所有元素 互不相同
+	1 <= target <= 40 */
+
+class Solution1 {
+public:
+	vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+		res.clear();
+		curr.clear();
+		n = candidates.size();
+		std::sort(candidates.begin(), candidates.end());
+		backtrack(candidates, 0, target);
+		return std::move(res);
+	}
+
+private:
+	int n;
+	vector<int> curr;
+	vector<vector<int>> res;
+	void backtrack(vector<int> &candidates, int i, int target) {
+		if(target <= 0) {
+			if(target == 0) {
+				res.push_back(curr);
+			}
+			return;
+		}
+		if(i == n) {
+			return;
+		}
+		if(target < candidates[i]) return;
+		curr.push_back(candidates[i]);
+		backtrack(candidates, i, target - candidates[i]);
+		curr.pop_back();
+		backtrack(candidates, i + 1, target);
+	}
+}; // 选或不选的 backtrack
+/*每个 i 都分裂成两支，递归树更“胖”
+剪枝点分散在多个 if
+状态由 (i, target) 驱动，不太贴合“组合枚举”的直觉*/
 
 class Solution {
 public:
 	vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
-		std::sort(candidates.begin(), candidates.end());
-		tar = target;
-		dfs(candidates, 0);
-		return res;
+		sort(candidates.begin(), candidates.end());
+		backtrack(candidates, 0, target);
+		return std::move(res);
 	}
 
 private:
 	vector<vector<int>> res;
 	vector<int> temp;
-	int sum = 0, tar;
-	void dfs(vector<int> &candidates, int start) {
-		if(sum > tar) {
-			return;
-		} else if(sum == tar) {
+	void backtrack(vector<int> &candidates, int start, int target) {
+		if(target == 0) {
 			res.emplace_back(temp);
 			return;
 		}
+
 		for(int i = start; i < candidates.size(); ++i) {
 			int val = candidates[i];
-			if(val + sum > tar) {
+			if(val > target) {
 				return;
-			}
+			} // 因为 candidates 已排序，后面只会更大
 			temp.emplace_back(val);
-			sum += val;
-			dfs(candidates, i);
+			backtrack(candidates, i, target - val);
 			temp.pop_back();
-			sum -= val;
 		}
 	}
-};
+}; // 每次至少选一个的 backtrack
+/*每一层递归代表“当前组合选下一个数”
+start 天然保证不重复组合
+排序 + val + sum > tar → 非常干净的剪枝
+递归树更窄，逻辑更贴合问题本质*/
 
 int main() {
 	Solution sol;
 	vector<int> candidates;
-	candidates = { 2, 3, 6, 7 };
-	int target = 7;
+	int target;
+
+	candidates = { 2, 3, 6, 7 }, target = 7;
+	cout << sol.combinationSum(candidates, target);
+
+	candidates = { 2, 3, 5 }, target = 8;
+	cout << sol.combinationSum(candidates, target);
+
+	candidates = { 2 }, target = 1;
 	cout << sol.combinationSum(candidates, target);
 }
--- 64N.minPathSum.cpp
+++ 64N.minPathSum.cpp
@@ -2,10 +2,20 @@
 #include "MyUtils.hpp"
 
 /*64. 最小路径和
-给定一个包含非负整数的 m x n 网格 grid
-，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
-说明：每次只能向下或者向右移动一步。*/
-// 从[0][0] 到 [m][n] 向下 i ++ 向右 j++
+/* 给定一个包含非负整数的 mxn网格grid，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+说明：每次只能向下或者向右移动一步。
+示例 1：
+	输入：grid = {{1,3,1},{1,5,1},{4,2,1}}
+	输出：7
+	解释：因为路径 1→3→1→1→1 的总和最小。
+示例 2：
+	输入：grid = {{1,2,3},{4,5,6}}
+	输出：12
+提示：
+	m == grid.length
+	n == grid{i}.length
+	1 <= m, n <= 200
+	0 <= grid{i}{j} <= 200 */
 
 class Solution1 {
 public:
@@ -39,10 +49,10 @@ public:
 	int minPathSum(vector<vector<int>> &grid) {
 		int m = grid.size(), n = grid[0].size();
 		for(int i = m - 2; i >= 0; --i) {
-			grid[i][n - 1] += grid[i + 1][n - 1];
+			grid[i].back() += grid[i + 1].back();
 		}
 		for(int j = n - 2; j >= 0; --j) {
-			grid[m - 1][j] += grid[m - 1][j + 1];
+			grid.back()[j] += grid.back()[j + 1];
 		}
 		for(int i = m - 2; i >= 0; --i) {
 			for(int j = n - 2; j >= 0; --j) {
@@ -55,6 +65,13 @@ public:
 
 int main() {
 	Solution sol;
-	vector<vector<int>> grid = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
-	cout << sol.minPathSum(grid);
+	vector<vector<int>> grid;
+	grid = { { 1, 2 }, { 1, 1 } };
+	cout << sol.minPathSum(grid) << endl;
+
+	grid = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+	cout << sol.minPathSum(grid) << endl;
+
+	grid = { { 1, 2, 3 }, { 4, 5, 6 } };
+	cout << sol.minPathSum(grid) << endl;
 }
