672dfe3ee2b07850e6e0643726dc6f5db43a8bf3
Aurora-73 <c2394842823@gmail.com>
2026-02-06 02:00:02 +0800

    Commit on 2026/02/06 周五

--- 128N.longestConsecutive.cpp
+++ 128N.longestConsecutive.cpp
@@ -1,10 +1,26 @@
-#include <string>
-#include <vector>
-#include <unordered_set>
 #include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-class Solution {
+/* 给定一个未排序的整数数组 nums，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+请你设计并实现时间复杂度为O(n) 的算法解决此问题。
+示例 1：
+	输入：nums = {100,4,200,1,3,2}
+	输出：4
+	解释：最长数字连续序列是 {1, 2, 3, 4}。它的长度为 4。
+示例 2：
+	输入：nums = {0,3,7,2,5,8,4,6,0,1}
+	输出：9
+示例 3：
+	输入：nums = {1,0,1,2}
+	输出：3
+提示：
+	0 <= nums.length <= 10^5
+	-10^9 <= nums{i} <= 10^9 */
+
+class Solution1 {
 public:
 	int longestConsecutive(vector<int> &nums) {
 		unordered_set<int> numset(nums.begin(), nums.end());
@@ -23,3 +39,36 @@ public:
 		return max;
 	}
 };
+
+class Solution {
+public:
+	int longestConsecutive(vector<int> &nums) {
+		unordered_set<int> vals(nums.begin(), nums.end());
+		int res = 0;
+		for(auto val : vals) { // 遍历 vals，自动去重
+			if(!vals.contains(val - 1)) {
+				int len = 1;
+				while(vals.contains(val + len)) ++len;
+				res = max(res, len);
+			}
+		}
+		return res;
+	}
+}; // 利用哈希表O(1)查找的性质，对于任何一个元素val，如果存在val-1则val一定不是最长连续序列的最小元素
+// 反之则val可能是最长连续序列的最小元素，不断寻找val + len，直到扩展到最大长度
+// 注意遍历时要在已经去重的哈希表中遍历，否则会多次重复查找相同元素，超时
+// 这道题的关键点有两个：1、哈希表O(1)查找   2、最长连续序列在原数组中的出现顺序不考虑
+
+int main() {
+	Solution sol;
+	vector<int> nums;
+
+	nums = { 100, 4, 200, 1, 3, 2 };
+	cout << sol.longestConsecutive(nums) << endl;
+
+	nums = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+	cout << sol.longestConsecutive(nums) << endl;
+
+	nums = { 1, 0, 1, 2 };
+	cout << sol.longestConsecutive(nums) << endl;
+}
---
+++ 144E.preorderTraversal.cpp
@@ -0,0 +1,78 @@
+// Created: 2026-02-06
+#include "MyTreeNode.h"
+#include "MyUtils.hpp"
+// #ifdef MY_UTILS_H
+
+/* 给你二叉树的根节点 root，返回它节点值的前序遍历。
+示例 1：
+	输入：root = {1,EMPTY_NODE,2,3}
+	输出：{1,2,3}
+	解释：
+示例 2：
+	输入：root = {1,2,3,4,5,EMPTY_NODE,8,EMPTY_NODE,EMPTY_NODE,6,7,9}
+	输出：{1,2,4,5,6,7,3,8,9}
+	解释：
+示例 3：
+	输入：root = {}
+	输出：{}
+示例 4：
+	输入：root = {1}
+	输出：{1}
+提示：
+	树中节点数目在范围 {0, 100} 内
+	-100 <= Node.val <= 100
+	进阶：递归算法很简单，你可以通过迭代算法完成吗？ */
+
+class Solution1 {
+public:
+	vector<int> preorderTraversal(TreeNode *root) {
+		preorder(root);
+		return std::move(res);
+	}
+
+private:
+	vector<int> res;
+	void preorder(TreeNode *root) {
+		if(!root) return;
+		res.push_back(root->val);
+		preorder(root->left);
+		preorder(root->right);
+	}
+};
+
+class Solution {
+public:
+	vector<int> preorderTraversal(TreeNode *root) {
+		vector<int> res;
+		stack<TreeNode *> st;
+		TreeNode *now = root;
+		while(now || st.size()) {
+			while(now) {
+				st.push(now);
+				res.push_back(now->val); // 和非递归中序遍历相比只是入栈时机不同
+				now = now->left;
+			}
+			now = st.top();
+			st.pop();
+			now = now->right;
+		}
+		return res;
+	}
+};
+
+int main() {
+	Solution sol;
+	TreeNode *root;
+
+	root = createTree({ 1, EMPTY_NODE, 2, 3 });
+	cout << sol.preorderTraversal(root) << endl;
+
+	root = createTree({ 1, 2, 3, 4, 5, EMPTY_NODE, 8, EMPTY_NODE, EMPTY_NODE, 6, 7, 9 });
+	cout << sol.preorderTraversal(root) << endl;
+
+	root = createTree({});
+	cout << sol.preorderTraversal(root) << endl;
+
+	root = createTree({ 1 });
+	cout << sol.preorderTraversal(root) << endl;
+}
